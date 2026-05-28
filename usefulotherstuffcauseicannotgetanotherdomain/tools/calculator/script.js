2d44768bc965135f582468ff111a8612ad6af3fe
Gha15 <husseinabedalii@gmail.com>
2026-05-28 11:51:34 +0300

    did something

---
+++ usefulotherstuffcauseicannotgetanotherdomain/tools/calculator/script.js
@@ -0,0 +1,8 @@
+let num1 = 0
+let num2 = 0
+let operator = ""
+let result = 0
+function additionofthetwonumbers() {
+    result = num1 + num2
+    document.getElementById("result").textContent = result
+}
