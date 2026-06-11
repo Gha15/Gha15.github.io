bb658b05d0cf3a0b213f7e69b29a940d5e73e791
Gha15 <husseinabedalii@gmail.com>
2026-06-11 19:56:59 +0300

    made first game

---
+++ games/script.js
@@ -0,0 +1,7 @@
+function opengame(game) {
+    // Implementation for opening a game
+    if (game === "the math plane") {
+        //open mathplanegame.html
+        window.location.href = "themathplanegame.html";
+    }
+}
