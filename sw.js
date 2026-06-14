a71c679b5879c8b2f3ca0d14bf77861f64a1d8c8
Gha15 <husseinabedalii@gmail.com>
2026-06-14 07:31:52 +0300

    made math calendar

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return new Response("You are offline. please connect to the mathernet");
-    })
-  );
-});
